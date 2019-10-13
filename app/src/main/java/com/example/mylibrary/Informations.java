3b592a2a48904d2ca75ad31a4b811f532770108b
esthcarelle <esthcarelle@gmail.com>
2019-10-13 20:42:23 +0200

    correcting indetation errors

--- app/src/main/java/com/example/mylibrary/Informations.java
+++ app/src/main/java/com/example/mylibrary/Informations.java
@@ -20,7 +20,6 @@ public class Informations extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.informations);
         mAddInfos=(Button) findViewById(R.id.addInfos);
