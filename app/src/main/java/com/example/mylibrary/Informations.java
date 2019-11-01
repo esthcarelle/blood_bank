475b85a62113c46f0fe84d1c6ed9a70bf1e22e53
esthcarelle <esthcarelle@gmail.com>
2019-11-01 17:02:52 +0200

    add some files

--- app/src/main/java/com/example/mylibrary/Informations.java
+++ app/src/main/java/com/example/mylibrary/Informations.java
@@ -1,18 +1,30 @@
 package com.example.mylibrary;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class Informations extends AppCompatActivity {
     private Button mAddInfos;
@@ -22,12 +34,17 @@ public class Informations extends AppCompatActivity {
 
     private DatabaseReference mNewInfos;
     private Button mSavedRestaurantsButton;
-
+    private Button uploadImage;
+    private ImageView nImageView;
+    public static final int CAMERA_REQUEST_CODE= 1;
+    private StorageReference mStorage;
+    private ProgressDialog mProgress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.informations);
+
 
         mNewInfos= FirebaseDatabase
                 .getInstance()
@@ -38,6 +55,11 @@ public class Informations extends AppCompatActivity {
         editBlood=(Spinner) findViewById(R.id.spinner1) ;
         editLocation=(EditText) findViewById(R.id.locationEditText) ;
         editName=(EditText) findViewById(R.id.nameEditText) ;
+       uploadImage=(Button) findViewById(R.id.takePicture);
+        nImageView=(ImageView) findViewById(R.id.picture);
+        mStorage= FirebaseStorage.getInstance().getReference();
+        mProgress=new ProgressDialog(this);
+
 
         String[] items = new String[]{"A", "B", "A+","B+","AB","AB+","O+","O-"};
         Spinner dropdown = findViewById(R.id.spinner1);
@@ -71,8 +93,39 @@ public class Informations extends AppCompatActivity {
             }
         });
 
+        uploadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,CAMERA_REQUEST_CODE);
+
+            }
+        });
 
     }
+    @Override
+    public void onActivityResult(int requestCode,int resultCode,Intent data){
+
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode==CAMERA_REQUEST_CODE && resultCode==RESULT_OK){
+
+            mProgress.setMessage("uploadinggggggg");
+            mProgress.show();
+            Uri uri=data.getData();
+            StorageReference filepath=mStorage.child("photos").child(uri.getLastPathSegment());
+            filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    mProgress.dismiss();
+
+                    Toast.makeText(Informations.this,"Upload finished",Toast.LENGTH_LONG).show();
+
+                }
+            });
+        }
+    }
+
+
 
     public void saveInfosToFirebase(SAveInformations infos) {
         mNewInfos.setValue(infos);
