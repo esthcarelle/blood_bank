9b8fe2db99ab3599219c4f16dd6e67a5f0ae2a0f
esthcarelle <esthcarelle@gmail.com>
2019-11-04 10:20:13 +0200

    add changes

--- app/src/main/java/com/example/mylibrary/Informations.java
+++ app/src/main/java/com/example/mylibrary/Informations.java
@@ -2,9 +2,11 @@ package com.example.mylibrary;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Base64;
 import android.view.View;
 
 import android.widget.ArrayAdapter;
@@ -20,11 +22,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
 
 public class Informations extends AppCompatActivity {
     private Button mAddInfos;
@@ -36,9 +41,11 @@ public class Informations extends AppCompatActivity {
     private Button mSavedRestaurantsButton;
     private Button uploadImage;
     private ImageView nImageView;
-    public static final int CAMERA_REQUEST_CODE= 1;
+    public static final int CAMERA_REQUEST_CODE = 1;
     private StorageReference mStorage;
     private ProgressDialog mProgress;
+    private static final int REQUEST_IMAGE_CAPTURE = 111;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -46,22 +53,22 @@ public class Informations extends AppCompatActivity {
         setContentView(R.layout.informations);
 
 
-        mNewInfos= FirebaseDatabase
+        mNewInfos = FirebaseDatabase
                 .getInstance()
                 .getReference()
                 .child(Constants.FIREBASE_CHILD_NEW_INFOS);
 
-        mAddInfos=(Button) findViewById(R.id.addInfos);
-        editBlood=(Spinner) findViewById(R.id.spinner1) ;
-        editLocation=(EditText) findViewById(R.id.locationEditText) ;
-        editName=(EditText) findViewById(R.id.nameEditText) ;
-       uploadImage=(Button) findViewById(R.id.takePicture);
-        nImageView=(ImageView) findViewById(R.id.picture);
-        mStorage= FirebaseStorage.getInstance().getReference();
-        mProgress=new ProgressDialog(this);
+        mAddInfos = (Button) findViewById(R.id.addInfos);
+        editBlood = (Spinner) findViewById(R.id.spinner1);
+        editLocation = (EditText) findViewById(R.id.locationEditText);
+        editName = (EditText) findViewById(R.id.nameEditText);
+        uploadImage = (Button) findViewById(R.id.takePicture);
+        nImageView = (ImageView) findViewById(R.id.picture);
+        mStorage = FirebaseStorage.getInstance().getReference();
+        mProgress = new ProgressDialog(this);
 
 
-        String[] items = new String[]{"A", "B", "A+","B+","AB","AB+","O+","O-"};
+        String[] items = new String[]{"A", "B", "A+", "B+", "AB", "AB+", "O+", "O-"};
         Spinner dropdown = findViewById(R.id.spinner1);
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, items);
         dropdown.setAdapter(adapter);
@@ -69,9 +76,9 @@ public class Informations extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String name = editName.getText().toString();
-                String location=editLocation.getText().toString();
-                String blood=editBlood.getSelectedItem().toString();
-                SAveInformations newInfos=new SAveInformations(name,location,blood);
+                String location = editLocation.getText().toString();
+                String blood = editBlood.getSelectedItem().toString();
+                SAveInformations newInfos = new SAveInformations(name, location, blood);
                 Intent intent = new Intent(Informations.this, Givers.class);
                 saveInfosToFirebase(newInfos);
                 intent.putExtra("location", location);
@@ -83,7 +90,7 @@ public class Informations extends AppCompatActivity {
 
 
         });
-        mSavedRestaurantsButton=(Button) findViewById(R.id.savedButton);
+        mSavedRestaurantsButton = (Button) findViewById(R.id.savedButton);
         mSavedRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -96,40 +103,66 @@ public class Informations extends AppCompatActivity {
         uploadImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent,CAMERA_REQUEST_CODE);
+//                onLaunchCamera();
 
             }
         });
 
     }
-    @Override
-    public void onActivityResult(int requestCode,int resultCode,Intent data){
+//    @Override
+//    public void onActivityResult(int requestCode,int resultCode,Intent data){
+//
+//        super.onActivityResult(requestCode,resultCode,data);
+//        if(requestCode==CAMERA_REQUEST_CODE && resultCode==RESULT_OK){
+//
+//            mProgress.setMessage("uploadinggggggg");
+//            mProgress.show();
+//            Uri uri=data.getData();
+//            StorageReference filepath=mStorage.child("photos").child(uri.getLastPathSegment());
+//            filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//                @Override
+//                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//                    mProgress.dismiss();
+//
+//                    Toast.makeText(Informations.this,"Upload finished",Toast.LENGTH_LONG).show();
+//
+//                }
+//            });
+//        }
 
-        super.onActivityResult(requestCode,resultCode,data);
-        if(requestCode==CAMERA_REQUEST_CODE && resultCode==RESULT_OK){
 
-            mProgress.setMessage("uploadinggggggg");
-            mProgress.show();
-            Uri uri=data.getData();
-            StorageReference filepath=mStorage.child("photos").child(uri.getLastPathSegment());
-            filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    mProgress.dismiss();
-
-                    Toast.makeText(Informations.this,"Upload finished",Toast.LENGTH_LONG).show();
-
-                }
-            });
-        }
-    }
 
 
 
     public void saveInfosToFirebase(SAveInformations infos) {
         mNewInfos.setValue(infos);
     }
+//    public void onLaunchCamera() {
+//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+//            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+//        }
+//    }
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == getActivity().RESULT_OK) {
+//            Bundle extras = data.getExtras();
+//            Bitmap imageBitmap = (Bitmap) extras.get("data");
+//            nImageView.setImageBitmap(imageBitmap);
+//            encodeBitmapAndSaveToFirebase(imageBitmap);
+//        }
+//    }
+//    public void encodeBitmapAndSaveToFirebase(Bitmap bitmap) {
+//        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+//        String imageEncoded = Base64.encodeToString(baos.toByteArray(), Base64.DEFAULT);
+//        DatabaseReference ref = FirebaseDatabase.getInstance()
+//                .getReference(Constants.FIREBASE_CHILD_CENTERS)
+//                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+//
+//                .child("imageUrl");
+//        ref.setValue(imageEncoded);
+//    }
 
 
 
--- app/src/main/java/com/example/mylibrary/MainActivity.java
+++ app/src/main/java/com/example/mylibrary/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.mylibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -15,50 +18,72 @@ import com.google.firebase.auth.FirebaseAuth;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
-    private Button getStartedButton;
+public class MainActivity extends Activity {
+//    private Button getStartedButton;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-
-
         setContentView(R.layout.activity_main);
 
-        getStartedButton=(Button) findViewById(R.id.getStarted);
-        getStartedButton.setOnClickListener(new View.OnClickListener() {
+        Button button=(Button) findViewById(R.id.getStarted);
+        Animation animatext = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fadeout);
+        button.startAnimation(animatext);
+
+
+
+
+//        getStartedButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//
+//
+//                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+        animatext.setAnimationListener(new Animation.AnimationListener() {
+
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
-                startActivity(intent);
+            public void onAnimationStart(Animation animatext) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animatext) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animatext) {
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));
             }
         });
+    }
 
 
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.men, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_logout) {
-            logout();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    private void logout() {
-        FirebaseAuth.getInstance().signOut();
-        Intent intent = new Intent(MainActivity.this, Informations.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
-        finish();
-    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.men, menu);
+//        return super.onCreateOptionsMenu(menu);
+//    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//        if (id == R.id.action_logout) {
+//            logout();
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+//    private void logout() {
+//        FirebaseAuth.getInstance().signOut();
+//        Intent intent = new Intent(MainActivity.this, Informations.class);
+//        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//        startActivity(intent);
+//        finish();
+//    }
 
 }
